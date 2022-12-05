cb78648a0872bccfdc3d04b6bbeba6544dca6e7c
Your Name <you@example.com>
2022-12-05 02:16:17 -0300

    comit

--- buscador/views.py
+++ buscador/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Item
 # Create your views here.
 def saludo(request, codigo_barras):
-    print(codigo_barras)
-    return HttpResponse("<h2>Hola carlossss<h2/>Codigo Buscado:  %s" %codigo_barras)
+    items = list(Item.objects.get(Barras=codigo_barras))
+    print(items)
+    return JsonResponse(items)
+
--- item/models.py
+++ item/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    id = models.CharField(max_length=20,primary_key=True)
-    Barras = models.IntegerField()
-    Codigo = models.CharField(max_length=20)
-    Relacional = models.CharField(max_length=20)
-    Nombre = models.CharField(max_length=200)
-    Precio = models.CharField(max_length=20)
-    Porcentaje = models.CharField(max_length=4)
-    Pack = models.CharField(max_length=20)
-    Final = models.CharField(max_length=20)
-    Trabajado = models.CharField(max_length=20)
-    Actualizado = models.CharField(max_length=20)
-    Fecha = models.CharField(max_length=20)
-    Stock = models.CharField(max_length=20)
-    Probeedores = models.CharField(max_length=20)
-    Cantidad = models.CharField(max_length=20)
-    Imagen = models.CharField(max_length=20)
