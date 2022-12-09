bb2f975278612c74e7407f87b567e9838e020a15
Your Name <you@example.com>
2022-12-09 00:02:40 -0300

    comit

--- actualizador/models.py
+++ actualizador/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Condicionales(models.Model):
+    tipo = models.CharField(max_length=20,primary_key=True)
+    Codigo = models.CharField(max_length=1)
+    Nombre = models.CharField(max_length=1)
+    Precio = models.CharField(max_length=1)
+    Porcentaje = models.FloatField()
+    Inicio = models.CharField(max_length=2)
+    Extension = models.CharField(max_length=20)
+    pass
+    
