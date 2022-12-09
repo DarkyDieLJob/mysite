7145b38932c823ced93e2524c018ca1b881ce2d5
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-09 11:05:35 -0300

    comit

--- buscador/admin.py
+++ buscador/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
-from django.contrib import admin
-from myapp.models import Author
-
-admin.site.register(Author)
+@admin.register(Item)
+class PersonAdmin(admin.ModelAdmin):
+    pass
