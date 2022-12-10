c577e3fcd756ef2d238810679b9b6cc50abd7d66
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-10 17:16:06 -0300

    comit

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Carrito
+from .models import Carrito, Finalizar_Venta
 
 # Register your models here.
 @admin.register(Carrito)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Finalizar_Venta)
 class PersonAdmin(admin.ModelAdmin):
     pass
--- planilla_diaria/admin.py
+++ planilla_diaria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Planilla_Diaria , Resumen_Diario
 
 # Register your models here.
+@admin.register(Planilla_Diaria)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Resumen_Diario)
+class PersonAdmin(admin.ModelAdmin):
+    pass
