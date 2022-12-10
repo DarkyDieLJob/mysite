8f19481e75ddf49c1d622adbc8d61240dbbc5664
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-10 09:39:11 -0300

    comit

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['darkydiel.pythonanywhere.com','localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
