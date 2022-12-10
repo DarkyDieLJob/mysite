9dc6b1fb528efc44e9770c2a3459ede1c329ed63
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-10 12:01:55 -0300

    comit

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Carrito
 
 # Register your models here.
+@admin.register(Carrito)
+class PersonAdmin(admin.ModelAdmin):
+    pass
--- carrito/models.py
+++ carrito/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-    
-    
-    
-    
-    
-    
+class Carrito(models.Model):
+    Usuario = models.CharField(max_length=20)
+    Codigo = models.CharField(max_length=20)
+    Nombre = models.CharField(max_length=20)
+    Cantidad = models.CharField(max_length=20)
+    Precio = models.CharField(max_length=20)
+    pass 
