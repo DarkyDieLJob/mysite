4dc847b54a0def48fc7c454062bfe42c1ea81ed5
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-10 17:33:53 -0300

    comit

--- proveedores/models.py
+++ proveedores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Lista_Pedidos(models.Model):
+    Proveedor = models.CharField(max_length=20)
+    Codigo_lista = models.CharField(max_length=20)
+    Nombre = models.CharField(max_length=20)
+    Cantidad = models.CharField(max_length=20)
+    pass
