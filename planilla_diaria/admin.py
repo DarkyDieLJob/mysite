db64767eade356e2a991cf3ba87bcac74d7d3c7d
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-10 17:27:43 -0300

    comit

--- planilla_diaria/admin.py
+++ planilla_diaria/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Planilla_Diaria , Resumen_Diario
+from .models import Planilla_Diaria , Resumen_Diario, Resumen_Mensual
 
 # Register your models here.
 @admin.register(Planilla_Diaria)
 class PersonAdmin(admin.ModelAdmin):
     pass
 @admin.register(Resumen_Diario)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Resumen_Mensual)
 class PersonAdmin(admin.ModelAdmin):
     pass
