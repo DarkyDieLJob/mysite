c80ea75a64a923e0e23cd134dec4c3cc2d81cce1
Your Name <you@example.com>
2022-12-10 21:25:18 -0300

    comit

--- actualizador/models.py
+++ actualizador/models.py
@@ -4,11 +4,38 @@ from django.db import models
 
 class Condicionales(models.Model):
     tipo = models.CharField(max_length=20,primary_key=True)
-    Codigo = models.CharField(max_length=1)
-    Nombre = models.CharField(max_length=1)
-    Precio = models.CharField(max_length=1)
-    Porcentaje = models.FloatField()
-    Inicio = models.CharField(max_length=2)
-    Extension = models.CharField(max_length=20)
+    codigo = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=1)
+    precio = models.CharField(max_length=1)
+    dolar = models.FloatField(max_length=29)
+    porcentaje = models.FloatField()
+    inicio = models.CharField(max_length=2)
+    extension = models.CharField(max_length=20)
     pass
+
+class Proveedores(models.Model):
+    tipo = models.CharField(max_length=20)
+    condicion = models.CharField(max_length=20)
+    dolar = models.FloatField(max_length=29)
+    fecha = models.CharField(max_length=20)
+    pass
+
+class Planilla(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
+    precio = models.CharField(max_length=20)
+    fecha = models.CharField(max_length=20)
+    moneda = models.CharField(max_length=20)
+    proveedor = models.CharField(max_length=20)
+    pass
+
+class Revision(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=20)
+    precio = models.CharField(max_length=20)
+    fecha = models.CharField(max_length=20)
+    moneda = models.CharField(max_length=20)
+    proveedor = models.CharField(max_length=20)
     
+    
+    pass
