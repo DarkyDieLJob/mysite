4e508d26747fd11bb316fc8995a1e938831bef8b
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-14 13:33:03 -0300

    comit

--- actualizador/resources.py
+++ actualizador/resources.py
@@ -1,5 +1,6 @@
 from import_export import resources  
-from .models import Prueva 
+from .models import Prueva
+ 
 class Prueva_Resource(resources.ModelResource):  
     class Meta:  
         model = Prueva
