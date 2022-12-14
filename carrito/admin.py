e925901fd14289e976d762c442b75e816fd5ec75
Your Name <you@example.com>
2022-12-14 01:38:46 -0300

    comit

--- carrito/admin.py
+++ carrito/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Carrito, Finalizar_Venta
+from .models import Items_Carrito, Carrito, Finalizar_Venta
 
 # Register your models here.
 @admin.register(Carrito)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Items_Carrito)
 class PersonAdmin(admin.ModelAdmin):
     pass
 @admin.register(Finalizar_Venta)
