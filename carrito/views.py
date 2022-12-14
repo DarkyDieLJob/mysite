d3f7069c23b3bea72f0e2207678a04703be2f0de
Your Name <you@example.com>
2022-12-14 01:32:15 -0300

    comit

--- carrito/views.py
+++ carrito/views.py
@@ -4,11 +4,16 @@ from .models import Carrito, Items_Carrito, Finalizar_Venta
 # Create your views here.
 def carrito(request):
     carrito = Carrito.objects.all()
-    
-    items = Items_Carrito.objects.all()
-    #manipular
-    items = items.values()
-    
+    try:
+        carrito = Carrito.objects.all()
+    except:
+        print("Hay un error en los valores de entrada")
+    try:
+        items = Items_Carrito.objects.all()
+        #manipular
+        items = items.values()
+    except:
+        print("Hay un error en los valores de entrada")
     
     
     return render(request, 'base_carrito.html', {
