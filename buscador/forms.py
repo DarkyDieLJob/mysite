b7c14df4bf3ed02b3188f55ef60c918fb108ff9b
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2022-12-15 14:40:08 -0300

    comit

--- buscador/forms.py
+++ buscador/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class Consultar_id(forms.Form):
-    id = forms.CharField(label=    "Codigo del producto.",max_length=20)
+    id = forms.CharField(label=    "Codigo del producto",max_length=20)
 class Consultar_Barras(forms.Form):
-    barras = forms.CharField(label="Codigo de barras   .",max_length=13)
+    barras = forms.CharField(label="Codigo de barras___",max_length=13)
 class Consultar_Nombre(forms.Form):
-    nombre = forms.CharField(label="Nombre             .",max_length=40)
+    nombre = forms.CharField(label="Nombre_____________",max_length=40)
