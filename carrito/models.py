65c696c686449a79f366548f249f8fe00d2a70b9
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2023-01-04 14:52:44 -0300

    comit

--- carrito/models.py
+++ carrito/models.py
@@ -2,24 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+from django.contrib.auth.models import User
+
 class Carrito(models.Model):
-    usuario = models.CharField(max_length=20)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     pass 
 
 class Items_Carrito(models.Model):
     carrito = models.ManyToManyField(Carrito)
     codigo = models.CharField(max_length=20)
     nombre = models.CharField(max_length=20)
-    precio = models.CharField(max_length=20)
-    cantidad = models.CharField(max_length=20)
+    precio = models.FloatField(default=0.0)
+    cantidad = models.FloatField(default=0.0)
     pass 
 
 class Finalizar_Venta(models.Model):
     nombre = models.CharField(max_length=20) #nombre de la entrada
     fecha = models.CharField(max_length=20)
     hora = models.CharField(max_length=20)
-    total = models.CharField(max_length=20)
+    total = models.FloatField(default=0.0)
     metodo_pago = models.CharField(max_length=20)
-    cuotas = models.CharField(max_length=20)
+    cuotas = models.IntegerField(default=1)
     estado = models.CharField(max_length=20)
     pass 
