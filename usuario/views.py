20515b6f42973028090e029c25f3be0e9b82c618
Matías Leandro López <98655953+DarkyDieLJob@users.noreply.github.com>
2023-01-04 22:06:54 -0300

    comit

--- usuario/views.py
+++ usuario/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+
 '''
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+@login_required
 class MyView(LoginRequiredMixin, View):
     
     
